410f0aa41a87592dd9e77a9ea06547c673979c42
sjameel98 <saadjameel98@gmail.com>
2019-03-18 00:38:30 -0400

    first comm

---
+++ regression.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import util
+
+def priorDistribution(beta):
+    """
+    Plot the contours of the prior distribution p(a)
+    
+    Inputs:
+    ------
+    beta: hyperparameter in the proir distribution
+    
+    Outputs: None
+    -----
+    """
+    ### TODO: Write your code here
+
+    
+    return 
+    
+def posteriorDistribution(x,z,beta,sigma2):
+    """
+    Plot the contours of the posterior distribution p(a|x,z)
+    
+    Inputs:
+    ------
+    x: inputs from training set
+    z: targets from traninng set
+    beta: hyperparameter in the proir distribution
+    sigma2: variance of Gaussian noise
+    
+    Outputs: 
+    -----
+    mu: mean of the posterior distribution p(a|x,z)
+    Cov: covariance of the posterior distribution p(a|x,z)
+    """
+    ### TODO: Write your code here
+   
+   
+    return (mu,Cov)
+
+def predictionDistribution(x,beta,sigma2,mu,Cov,x_train,z_train):
+    """
+    Make predictions for the inputs in x, and plot the predicted results 
+    
+    Inputs:
+    ------
+    x: new inputs
+    beta: hyperparameter in the proir distribution
+    sigma2: variance of Gaussian noise
+    mu: output of posteriorDistribution()
+    Cov: output of posteriorDistribution()
+    x_train,z_train: training samples, used for scatter plot
+    
+    Outputs: None
+    -----
+    """
+    ### TODO: Write your code here
+    
+    
+    return 
+
+if __name__ == '__main__':
+    
+    # training data
+    x_train, z_train = util.get_data_in_file('training.txt')
+    # new inputs for prediction 
+    x_test = [x for x in np.arange(-4,4.01,0.2)]
+    
+    # known parameters 
+    sigma2 = 0.1
+    beta = 1
+    
+    # number of training samples used to compute posterior
+    ns  = 5
+    
+    # used samples
+    x = x_train[0:ns]
+    z = z_train[0:ns]
+    
+    # prior distribution p(a)
+    priorDistribution(beta)
+    
+    # posterior distribution p(a|x,z)
+    mu, Cov = posteriorDistribution(x,z,beta,sigma2)
+    
+    # distribution of the prediction
+    predictionDistribution(x_test,beta,sigma2,mu,Cov,x,z)
+    
+
+   
+
+    
+    
+    
+
+    
